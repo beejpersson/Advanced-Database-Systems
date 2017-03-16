961ec90b1190b76561df6a45edaded90fbdfc4a0
Beej Persson <beejpersson@btinternet.com>
2017-03-16 23:44:04 +0000

    Added data for account table

--- Database Creation SQL.sql
+++ Database Creation SQL.sql
@@ -102,6 +102,7 @@ CREATE TABLE accountTable OF account_typ (
 	CONSTRAINT accType_const CHECK(accType IN ('current', 'savings')),
 	CONSTRAINT balance_const CHECK(balance IS NOT NULL),
 	CONSTRAINT inRate_const CHECK(inRate IS NOT NULL),
+    CONSTRAINT limitOfFreeOD_const CHECK(limitOfFreeOD IS NOT NULL),
 	CONSTRAINT openDate_const CHECK(openDate IS NOT NULL));
 	
 /
@@ -139,10 +140,11 @@ CREATE TABLE employeeTable OF employee_typ (
 	CONSTRAINT ePosition_const CHECK (eJob.position IN ('Head', 'Manager', 'Accountant', 'Leader', 'Cashier')),
 	CONSTRAINT eSalary_const CHECK(eJob.salary IS NOT NULL),
 	CONSTRAINT eJoinDate_const CHECK(eJob.joinDate IS NOT NULL))
-    NESTED TABLE pPhone.mobilePhone STORE AS eM obilePhones_nested_table;
+    NESTED TABLE pPhone.mobilePhone STORE AS eMobilePhones_nested_table;
 	
 /
 
+--Insert Data into Branch Table
 insert into branchTable values (1, address_typ('Westerfield', 'Zhenghu', 'WN8 OW0'), '86-(411)478-4891');
 insert into branchTable values (2, address_typ('Commercial', 'Santo Antônio do Monte', 'BD9 HG2'), '55-(243)128-8960');
 insert into branchTable values (3, address_typ('Merry', 'Alexandria', 'ZP6 AI0'), '20-(591)387-2220');
@@ -163,3 +165,25 @@ insert into branchTable values (17, address_typ('Northfield', 'Rey', 'ZS6 DK4'),
 insert into branchTable values (18, address_typ('Moland', 'Samashki', 'AG6 JX8'), '7-(317)754-6463');
 insert into branchTable values (19, address_typ('Chive', 'Vereya', 'GD0 IQ8'), '7-(840)591-1520');
 insert into branchTable values (20, address_typ('Portage', 'San José', 'QU8 KP4'), '506-(220)966-3351');
+
+--Insert Data into Account Table
+insert into accountTable values (101, 'savings', 6376.8, (SELECT REF(b) FROM branchTable b WHERE b.bID = 1), 0.056, 497, '14-Dec-2000');
+insert into accountTable values (102, 'savings', 5798.7, (SELECT REF(b) FROM branchTable b WHERE b.bID = 17), 0.197, 599, '19-Nov-2000');
+insert into accountTable values (103, 'current', 6922.42, (SELECT REF(b) FROM branchTable b WHERE b.bID = 5), 0.021, 107, '13-Apr-2004');
+insert into accountTable values (104, 'savings', 4780.39, (SELECT REF(b) FROM branchTable b WHERE b.bID = 1), 0.191, 123, '12-Jul-2011');
+insert into accountTable values (105, 'current', 213.12, (SELECT REF(b) FROM branchTable b WHERE b.bID = 6), 1.798, 67, '15-May-2005');
+insert into accountTable values (106, 'savings', 4047.7, (SELECT REF(b) FROM branchTable b WHERE b.bID = 19), 1.748, 331, '28-Sep-2005');
+insert into accountTable values (107, 'savings', 3756.8, (SELECT REF(b) FROM branchTable b WHERE b.bID = 8), 1.573, 676, '02-Mar-2001');
+insert into accountTable values (108, 'current', 2336.28, (SELECT REF(b) FROM branchTable b WHERE b.bID = 2), 0.652, 694, '03-Jul-2001');
+insert into accountTable values (109, 'current', 9219.34, (SELECT REF(b) FROM branchTable b WHERE b.bID = 3), 0.713, 528, '13-Oct-2006');
+insert into accountTable values (110, 'savings', 7693.28, (SELECT REF(b) FROM branchTable b WHERE b.bID = 8), 0.698, 261, '30-Mar-2016');
+insert into accountTable values (111, 'current', 2059.5, (SELECT REF(b) FROM branchTable b WHERE b.bID = 15), 1.94, 981, '19-Dec-2003');
+insert into accountTable values (112, 'savings', 737.47, (SELECT REF(b) FROM branchTable b WHERE b.bID = 14), 0.052, 696, '10-Aug-2006');
+insert into accountTable values (113, 'savings', 7126.53, (SELECT REF(b) FROM branchTable b WHERE b.bID = 18), 0.527, 157, '28-Sep-2012');
+insert into accountTable values (114, 'savings', 2849.39, (SELECT REF(b) FROM branchTable b WHERE b.bID = 4), 0.907, 145, '28-Feb-2014');
+insert into accountTable values (115, 'current', 7880.63, (SELECT REF(b) FROM branchTable b WHERE b.bID = 6), 1.769, 852, '08-Nov-2011');
+insert into accountTable values (116, 'savings', 2178.58, (SELECT REF(b) FROM branchTable b WHERE b.bID = 2), 0.615, 112, '29-Mar-2000');
+insert into accountTable values (117, 'current', 6568.43, (SELECT REF(b) FROM branchTable b WHERE b.bID = 13), 0.047, 115, '24-Jun-2011');
+insert into accountTable values (118, 'savings', 2552.88, (SELECT REF(b) FROM branchTable b WHERE b.bID = 16), 1.901, 51, '11-May-2012');
+insert into accountTable values (119, 'current', 2878.95, (SELECT REF(b) FROM branchTable b WHERE b.bID = 11), 1.952, 927, '08-Oct-2006');
+insert into accountTable values (120, 'savings', 1109.63, (SELECT REF(b) FROM branchTable b WHERE b.bID = 7), 0.725, 854, '27-Nov-2009');

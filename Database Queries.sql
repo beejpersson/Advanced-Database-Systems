a796d7801a1399b7c7fba2f67e4951a8d7045675
Beej Persson <beejpersson@btinternet.com>
2017-03-17 10:55:55 +0000

    fixed errors in 4a, attempt at 4b, updated final doc

--- Database Queries.sql
+++ Database Queries.sql
@@ -1,4 +1,9 @@
 --Question 4a.
 --Find employees with 'on' in first name who live in glasgow (3 marks)
 
-SELECT e.pName.firstName, e.pName.surName FROM employeeTable e WHERE e.pAddress.city = 'Glasgow' AND e.pName.firstName LIKE '$on$';
+SELECT e.pName.firstName AS "First Name", e.pName.surName AS "Last Name" FROM employeeTable e WHERE e.pAddress.city = 'Glasgow' AND e.pName.firstName LIKE '$on$';
+
+--Question 4b.
+--Find the number of savings account at each branch (3 marks)
+
+SELECT count(a.accType) AS "Number of Savings Accounts", a.bID.bAddress.street AS "Branch Street", a.bID.bAddress.city AS "Branch City", a.bID.bAddress.p_code AS "Branch Post Code" FROM accountTable a WHERE a.accType = 'savings';
